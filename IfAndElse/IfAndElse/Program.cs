87c83a78c6dc3039b914f67fe52169e0e1fa1d9d
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-10 09:47:45 +0300

    IfAndElse

---
+++ IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace IfAndElse
+{
+    //Projekt nimetusega IfAndElse on, mille sees asub
+    //class nimega  Program. See klass sisaldab Main meetodit,
+    //mis on programmi sisenemispunkt
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta enda nimi:");
+
+            //muutuja nimega name, kuhu salvestatakse kasutaja
+            //sisestatud tekst
+            string name = Console.ReadLine();
+
+            //! tähendab "ei ole" ja == tähendab "on võrdne"
+            if (name != " ")
+            {
+                //Kui kasutajasisestab ,idagi, siis muudetakse taustavärv
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Sisestasid enda nime");
+                Console.WriteLine(name);
+            }
+            else
+            {
+                Console.WriteLine("Sisestasid tühja nime");
+                //Kui kasutaja ei sisesta midagi, siis kostub 2 piiksu
+                Console.Beep();
+                //ja oodatakse 1 sekund enne programmi lõppu
+                Thread.Sleep(1000);
+                Console.Beep();
+            }  
+        }
+    }
+}
