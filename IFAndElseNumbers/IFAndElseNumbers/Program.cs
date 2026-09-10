8d94aaf7b120d357713490a0e85ba4559bb40d32
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-10 11:00:36 +0300

    IFAndElseNumbers

---
+++ IFAndElseNumbers/IFAndElseNumbers/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace IFAndElseNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number");
+
+            //konsoolis sisestame numbri
+            //see number salvestatakse string andmetüüpi
+            //toimub kontroll, kas sisestatud väärtus on number
+            //kasutada if ja else lauseid
+
+            string Input = Console.ReadLine();
+
+            if(int.TryParse(Input, out int number))
+            {
+                Console.WriteLine($"Sisestatud number on: {number}");
+            }
+            else
+            {
+            Console.WriteLine("Sisestatud väärtus ei ole number.");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        }
+}
