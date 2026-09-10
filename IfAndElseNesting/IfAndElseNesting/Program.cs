7a31dda6d10709683d047c48747fde86e2eeb95b
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-10 11:28:48 +0300

    IfandElseNesting

---
+++ IfAndElseNesting/IfAndElseNesting/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace IfAndElseNesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If and else nesting");
+            //Nesting tähendab, et if-else struktuuride
+            //sees võib olla teisi if-else struktuure
+            //See võimaldab teha keerukamaid ostuseid ja
+            //kontrollida erinevaid tingimusi
+
+            int number = 11;
+            if (number == 9)
+            {
+                //kui sisestan 9, siis see if-else struktuur käivitub
+                if (number == 11)
+                {
+                    //kui sisestan 11, siis see if-else struktuur käivitub
+                }
+                else
+                {
+                    Console.WriteLine("Vastus oli 0 kuni 10 ja 12 kuni 19");
+                }
+            }
+            else if (number == 21)
+            {
+                Console.WriteLine("Vastus oli 21");
+            }
+            else if (number == 21)
+            {
+                Console.WriteLine("Vastus oli 30");
+            }
+            else
+            {
+                Console.WriteLine("Mingi kahtlane number");
+            }
+        }   
+    }
+}
