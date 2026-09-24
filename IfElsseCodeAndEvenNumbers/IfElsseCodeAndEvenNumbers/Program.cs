8dd7052b33d6e7ac12169c0f5f0cac9b14e249c0
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-24 11:43:44 +0300

    IfElseOddAndEven

---
+++ IfElsseCodeAndEvenNumbers/IfElsseCodeAndEvenNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Xml.Serialization;
+
+namespace IfElsseCodeAndEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //konsool küsib numbrit
+            //number tuleb ära parsida
+
+            //if ja else juures toimub kontroll, et 
+            //kas on paaris või paaritu nr
+
+            string nr = Console.ReadLine();
+            int number = int.Parse(nr);
+
+            //% jagab muutujat number 2 
+            //kui 2 asemel panan 3, siis jagab 3-ga
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("See on paarisarv" + number);
+                Paarisarv();
+                //siia tuleb välja kutsuda meetod,
+                //mis ütleb, et see on paarisarv
+            }
+            else
+            {
+                Console.WriteLine("See on paaritu number" + number );
+                Paarisarv();
+                //siia tuleb välja kutsuda meetod,
+                //mis ütleb, et see on paarituarv
+            }
+        }
+        static void Paarituarv()
+        {
+            Console.WriteLine("Antudnumber on paaritu arv");
+        }
+        static void Paarisarv()
+        {
+            Console.WriteLine("Antud number on paarisarv");
+        }
+    }
+}
